a08b01a265bf64592ffe2c5dbd57bb52ed973a13
nekosu <liao.hy@outlook.com>
2023-01-12 22:46:46 +0800

    [feat] Support deploy

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -8,13 +8,11 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-	],
+  plugins: [vue(), vuetify({ autoImport: true })],
+  base: '/sctavern/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
