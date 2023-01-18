1c5aaefd3202069f0b8f56a8806fa64ef2b10667
neko-para <liao.hy@outlook.com>
2023-01-18 07:09:30 +0000

    Deploying to gh-pages from @ neko-para/sctavern@831c4ff6435fac320aba7da53660ab61d20f0e5f 🚀

--- assets/RootMenu-d1b8045d.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as o,u as t,o as n}from"./index-55be30ac.js";const p=e({__name:"RootMenu",setup(r){return t().push({name:"local-config"}),(c,s)=>(n(),o("div"))}});export{p as default};
