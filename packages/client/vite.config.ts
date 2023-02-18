4abf71065ef9a0c4c9b06a31663ef3d28cf8cab9
nekosu <liao.hy@outlook.com>
2023-02-18 23:19:36 +0800

    [refactor] Rename packages

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,18 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
-  base: '/sctavern/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  plugins: [react()],
 })
