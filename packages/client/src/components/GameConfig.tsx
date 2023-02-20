efcac4183c3d8619edb4d90033001f297feabe4a
nekosu <liao.hy@outlook.com>
2023-02-21 00:51:56 +0800

    [feat] Finish config

--- packages/client/src/components/GameConfig.tsx
+++ packages/client/src/components/GameConfig.tsx
@@ -1,10 +1,19 @@
 import {
+  AllRole,
+  ExtPack,
+  PackKey,
   PresetPoolPack,
+  PvePresetActivePack,
+  PvePresetActiveUnit,
   PvpPresetActivePack,
   PvpPresetActiveUnit,
+  RoleData,
+  RoleKey,
 } from '@sctavern/data'
-import type { GameConfig } from '@sctavern/emulator'
-import { redirect, useNavigate } from 'react-router'
+import { type GameConfig, LCG } from '@sctavern/emulator'
+import { useNavigate } from 'react-router'
+
+const lcg = new LCG(Math.floor(Math.random() * 100000))
 
 function GameConfig() {
   const [config, setConfig] = useState<GameConfig>({
@@ -20,11 +29,41 @@ function GameConfig() {
 
   const navigate = useNavigate()
 
+  const handlePackChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const packs = event.target.checked
+      ? config.Pack.concat([event.target.name as PackKey])
+      : config.Pack.filter(p => p !== event.target.name)
+    setConfig({
+      ...config,
+      Pack: packs,
+    })
+  }
+
+  const allRole = AllRole.filter(r => !RoleData[r].ext)
+
+  const handlePveChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setConfig({
+        ...config,
+        Pve: true,
+        ActivePack: PvePresetActivePack,
+        ActiveUnit: PvePresetActiveUnit,
+      })
+    } else {
+      setConfig({
+        ...config,
+        Pve: false,
+        ActivePack: PvpPresetActivePack,
+        ActiveUnit: PvpPresetActiveUnit,
+      })
+    }
+  }
+
   return (
     <Container>
       <CardView>
         <CardContent className="InfoCard">
-          <Box display="grid" gridTemplateColumns="repeat(12, 1fr)">
+          <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" rowGap={2}>
             <Box gridColumn="span 1"></Box>
             <Box gridColumn="span 8">
               <TextField
@@ -59,6 +98,106 @@ function GameConfig() {
               </Button>
             </Grid>
             <Box gridColumn="span 1"></Box>
+            <Box gridColumn="span 1"></Box>
+            <Box
+              gridColumn="span 8"
+              display="grid"
+              gridTemplateColumns="repeat(2, 1fr)"
+            >
+              {ExtPack.map((pack, index) => {
+                return (
+                  <FormControlLabel
+                    key={index}
+                    label={pack}
+                    control={
+                      <Checkbox
+                        name={pack}
+                        checked={config.Pack.includes(pack)}
+                        onChange={handlePackChange}
+                      ></Checkbox>
+                    }
+                  ></FormControlLabel>
+                )
+              })}
+            </Box>
+            <Grid
+              gridColumn="span 2"
+              container
+              justifyContent="center"
+              alignContent="center"
+            >
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setConfig({
+                    ...config,
+                    Pack: [
+                      '核心',
+                      ...lcg.shuffle(ExtPack.map(x => x)).slice(0, 2),
+                    ],
+                  })
+                }}
+              >
+                随机
+              </Button>
+            </Grid>
+            <Box gridColumn="span 1"></Box>
+            <Box gridColumn="span 1"></Box>
+            <Box gridColumn="span 8">
+              <FormControl fullWidth>
+                <InputLabel>角色</InputLabel>
+                <Select
+                  label="角色"
+                  value={config.Role[0]}
+                  onChange={event => {
+                    setConfig({
+                      ...config,
+                      Role: [event.target.value as RoleKey],
+                    })
+                  }}
+                >
+                  {allRole.map((role, index) => {
+                    return (
+                      <MenuItem value={role} key={index}>
+                        {role}
+                      </MenuItem>
+                    )
+                  })}
+                </Select>
+              </FormControl>
+            </Box>
+            <Grid
+              gridColumn="span 2"
+              container
+              justifyContent="center"
+              alignContent="center"
+            >
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setConfig({
+                    ...config,
+                    Role: [lcg.one_of(allRole) as RoleKey],
+                  })
+                }}
+              >
+                随机
+              </Button>
+            </Grid>
+            <Box gridColumn="span 1"></Box>
+            <Box gridColumn="span 1"></Box>
+            <Box gridColumn="span 4">
+              <FormControlLabel
+                label="启用PVE"
+                control={
+                  <Checkbox
+                    name="启用PVE"
+                    checked={config.Pve}
+                    onChange={handlePveChange}
+                  ></Checkbox>
+                }
+              ></FormControlLabel>
+            </Box>
           </Box>
         </CardContent>
         <CardActions>
